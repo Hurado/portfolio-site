2590a5ce74d7ea89dad93a39e60c5449a2deee8d
Hurado <piaszczynskiarkadiusz@gmail.com>
2022-12-28 23:08:42 +0100

    feat: :zap: Added a image of github in footer which navigate to personal github

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
+
 const Footer = () => {
+  const icon = <FontAwesomeIcon icon={faGithub} />;
   return (
     <div className="border-t border-slate-300 p-4 text-slate-400">
       <div className="text-center">Designed and built by Arkadiusz</div>
+      <Link href="https://github.com/Hurado">
+        <div className="text-3xl text-slate-300 text-center">{icon}</div>
+      </Link>
     </div>
   );
 };
